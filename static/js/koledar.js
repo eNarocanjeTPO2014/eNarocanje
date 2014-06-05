931ce765181976b74d2eb5036f53e135c85398f3
mitjari <mitjarizvic@gmail.com>
2014-06-05 06:34:43 -0700

    Dokončani prevodi.

--- static/js/koledar.js
+++ static/js/koledar.js
@@ -44,6 +44,17 @@ function readParameters ()
 
 function renderCalendar()
 {
+    gumbi= document.getElementById('calendar_trans').value
+    gumbi= gumbi.split(" ")
+
+    dnevi= document.getElementById('day_trans').value
+    dnevi= dnevi.split(" ")
+
+    dnevis= document.getElementById('day_trans_s').value
+    dnevis= dnevis.split(" ")
+
+    alert(dnevi[0])
+
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -53,6 +64,18 @@ function renderCalendar()
 
         /* sets the current view */
         defaultView: (document.width >= 800) ? 'agendaWeek' : 'agendaDay',
+
+
+        /* translation */
+        buttonText:{
+            today: gumbi[0],
+            month: gumbi[1],
+			week: gumbi[2],
+			day: gumbi[3]
+        },
+
+        dayNames: [dnevi[0], dnevi[1], dnevi[2], dnevi[3], dnevi[4], dnevi[5], dnevi[6]],
+        dayNamesShort:[dnevis[0], dnevis[1], dnevis[2], dnevis[3], dnevis[4], dnevis[5], dnevis[6]],
 
 
         editable: false,
